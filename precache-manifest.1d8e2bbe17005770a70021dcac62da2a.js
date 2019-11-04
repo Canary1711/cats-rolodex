367149edc5313b4bfe54e276dc1003c42b650cc1
Peter Wenji <pieterjake321@gmail.com>
2019-11-04 12:44:09 +0700

    Updates

---
+++ precache-manifest.1d8e2bbe17005770a70021dcac62da2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44254cdc453ffae1283ca2eadfbf33b6",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "c43647b5d144b0040bde",
+    "url": "/cats-rolodex/static/css/main.5a7e1797.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/cats-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "c43647b5d144b0040bde",
+    "url": "/cats-rolodex/static/js/main.1991c75e.chunk.js"
+  },
+  {
+    "revision": "37c3f2fe981555e9fdbd",
+    "url": "/cats-rolodex/static/js/runtime-main.98d6f816.js"
+  }
+]);
